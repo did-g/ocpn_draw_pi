70432653728547c8552e1d51f533ad38e2a002d4
Jon <jon.gough@eclipsesystems.com.au>
2018-01-03 08:41:00 +1100

    Corrected delete of config settings Re worked wording to make clearer what is happening Allow user to undo change if OCPN has not been closed
    Conflicts:
    	src/ODPropertiesDialogImpl.cpp
    	src/ocpn_draw_pi.cpp

--- src/ODPropertiesDialogImpl.cpp
+++ src/ODPropertiesDialogImpl.cpp
@@ -1011,6 +1011,11 @@ void ODPropertiesDialogImpl::UpdateProperties( void )
     m_textCtrlDRPointInterval->SetValue( wxString::Format( _T("%.3f"), g_dDRPointInterval ) );
     
 
+    if(g_ocpn_draw_pi->m_bRecreateConfig) {
+        m_buttonConfigFileEntries->SetLabel(_("No"));
+        m_staticTextConfigFileEntriesMsg->SetLabel("Delete current OCPN_Draw config entries (Yes)");
+    }
+    
     SetDialogSize();
 
     return;
@@ -1051,5 +1056,13 @@ void ODPropertiesDialogImpl::SetTableCellBackgroundColours()
 
 void ODPropertiesDialogImpl::OnClickConfigFileEntries( wxCommandEvent& event )
 {
-    g_ocpn_draw_pi->RecreateConfig();
+    if(!g_ocpn_draw_pi->m_bRecreateConfig) {
+        g_ocpn_draw_pi->m_bRecreateConfig = true;
+        m_buttonConfigFileEntries->SetLabel(_("No"));
+        m_staticTextConfigFileEntriesMsg->SetLabel("Delete current OCPN_Draw config entries (Yes)");
+    } else {
+        g_ocpn_draw_pi->m_bRecreateConfig = false;
+        m_buttonConfigFileEntries->SetLabel(_("Yes"));
+        m_staticTextConfigFileEntriesMsg->SetLabel("Delete current OCPN_Draw config entries (No)");
+    }
 }
